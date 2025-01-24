40d17045948244df17a6efc7f732daed37f9167e
SunMouren <yhvy5ufhb865@outlook.com>
2025-01-24 22:48:22 +0800

    modified:   Router/Download.js

--- Router/Download.js
+++ Router/Download.js
@@ -11,37 +11,38 @@ const log = require("../CreateLog")
 const { json } = require("stream/consumers")
 
 module.exports = router.get("/download/:key", (req, res) => {
-    const db = new sqlite3.Database(path.join(__dirname,"../","access","db","File.db"))
+    const db = new sqlite3.Database(path.join(__dirname, "../", "access", "db", "File.db"))
     db.all("select * from fileinfo where filekey = ?", [req.params.key], (error, rows) => {
         if (error) {
             console.log(log.OutputAndWrite(error.message))
         }
-        console.log(log.OutputAndWrite(`Create download task:\n`+JSON.stringify(rows)))
+        console.log(log.OutputAndWrite(`Create download task`))
         if (rows[0] == null) {
             res.send("文件不存在")
             return
         } else {
-            fs.renameSync(rows[0]["Filepath"], path.join(__dirname,"../","access","file", rows[0]["Filename"]), (error) => {
+            fs.renameSync(rows[0]["Filepath"], path.join(__dirname, "../", "access", "file", rows[0]["Filename"]), (error) => {
                 if (error) {
                     console.error(log.OutputAndWrite(error.message))
                 }
             })
-            res.download(path.join(__dirname,"../","access","file", rows[0]["Filename"]), (error) => {
+            
+            res.download(path.join(__dirname, "../", "access", "file", rows[0]["Filename"]), (error) => {
                 if (error) {
                     console.log(log.OutputAndWrite(error.message))
-                } else {
-                    fs.rmSync(path.join(__dirname, "file", rows[0]["Filename"]), { force: true })
-                    db.run("delete from fileinfo where filekey = ?", [req.params.key], (error) => {
-                        if(error){
-                            console.error(log.OutputAndWrite(error.message))
-                        }
-                    })
-                    
                 }
+                console.log(log.OutputAndWrite(rows[0]["Filename"]))
+                fs.rmSync(path.join(__dirname,"../", "access", "file", rows[0]["Filename"]), { force: true })
+                db.run("delete from fileinfo where filekey = ?", [req.params.key], (error) => {
+                    if (error) {
+                        console.error(log.OutputAndWrite(error.message))
+                    }
+                })
+
+
             })
 
 
         }
     })
-    db.close()
 })
